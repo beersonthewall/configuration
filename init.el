028f4499879e6d497a84f22ee22c15dd3cd25f86
beersonthewall <matthew@beersonthewall.com>
2020-11-01 19:55:50 -0600

    Emacs configuration.

---
+++ init.el
@@ -0,0 +1,33 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
+;; and `package-pinned-packages`. Most users will not need or want to do this.
+;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
+(package-refresh-contents)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (rust-mode json-mode go-mode markdown-mode yaml-mode))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 4)
+(add-hook 'go-mode-hook 'lsp-deferred)
+(setq gofmt-command "goimports")
+(setq column-number-mode t)
+(transient-mark-mode 1)
+(set-face-attribute 'region nil :background "#666")
+(add-hook 'json-mode-hook
+          (lambda ()
+            (make-local-variable 'js-indent-level)
+            (setq js-indent-level 2)))
+
